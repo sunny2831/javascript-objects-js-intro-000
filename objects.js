bda6174ff467397b1bd2830cdec4d91e8215cb70
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-13 17:37:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,8 +8,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
+<<<<<<< HEAD
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
   return playlist
 }
+=======
+>>>>>>> 37e748bd9a23cbeb208527cb1acc812f883ab56b
